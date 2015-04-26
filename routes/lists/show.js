7659e0d4aa93d87ebf5d58850dfb755fdf55e01e
Jessica H Lee <jessica.hyejin@gmail.com>
2015-04-26 11:11:35 -0700

    Rendering lists/show on todo, using outlets

--- routes/lists/show.js
+++ routes/lists/show.js
@@ -11,62 +11,8 @@ export default Ember.Route.extend({
 		this.render('todos', {
 			into: 'lists/show',
 			outlet: 'todos',
+			controller: 'todo'
 		});
-	},
-	
-
-	actions: {
-		createTodo: function() {
-			var newTodoTitle = this.controllerFor(this.routeName).get('newTodoTitle');
-
-			if (Ember.isBlank(newTodoTitle)) {return false;}
-
-			var list = this.modelFor(this.routeName);
-
-			var todo = this.store.createRecord('todo', {
-				title: newTodoTitle,
-				list: list
-			});
-
-			this.controllerFor(this.routeName).set('newTodoTitle', '');
-
-			todo.save().then(function(todo) {
-				list.get('todos').addObject(todo);
-				list.save();
-			});
-		},
-
-
-		deleteTodo: function(id) {
-			var list = this.modelFor(this.routeName);
-
-			this.store.find('todo', id).then(function(todo) {
-				list.get('todos').removeObject(todo);
-				list.save();
-
-				todo.destroyRecord();
-			});
-		},
-
-
-		deleteList: function() {
-			var list = this.modelFor(this.routeName);
-			list.destroyRecord();
-
-			this.transitionTo('lists');
-		},
-
-
-		updateTitle: function() {
-			var model = this.modelFor(this.routeName);
-
-			if (Ember.isBlank(model.get('title'))) {
-				model.rollback();
-			}
-			else {
-				model.save();
-			}
-		}
 	},
 
 	isEditing: false,
