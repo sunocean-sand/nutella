c88c7c3d814d51a984b5f3a9740329cc95ecd006
Jessica H Lee <jessica.hyejin@gmail.com>
2015-04-28 20:29:09 -0700

    edited models to match user authentication definitions

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	name: DS.attr('string'),
+	displayName: DS.attr('string'),
 	email: DS.attr('string'),
 
 	list: DS.hasMany('list', {async: true}),
